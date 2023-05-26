3bcbafc23c14a003bd8a3daf2a130be17b671463
Emmie C <z5360408@ad.unsw.edu.au>
2023-05-26 22:18:43 +1000

    Innitialised routes

---
+++ server/server.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import taskRoute from './routes/taskRoute';
+import tasksRoute from './routes/tasksRoute';
+import tagRoute from './routes/tagRoute';
+import tagsRoute from './routes/tagsRoute';
+
+const app = express();
+const PORT = 3001;
+
+app.get("/", (req, res) => {
+    console.log("Backend running at root route");
+    return res.json({msg: "Backend running at root route"});
+})
+
+app.use("/task", taskRoute);
+
+app.use("/tasks", tasksRoute);
+
+app.use("/tag", tagRoute);
+
+app.use("/tags", tagsRoute);
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+})
