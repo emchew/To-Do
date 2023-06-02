528f958d136734f98d80640bff8af3b569164b2d
Emmie C <z5360408@ad.unsw.edu.au>
2023-06-02 20:34:17 +1000

    Removed debugging statement from server

--- server/routes/tagsRoute.ts
+++ server/routes/tagsRoute.ts
@@ -4,8 +4,6 @@ import { tagsList } from "../src/tag";
 const router = express.Router();
 
 router.get("/list", (req, res) => {
-    console.log(tagsList());
-    console.log("im trying i swear")
     return res.json(tagsList());
 });
 
