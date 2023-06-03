1562fe3f699065141665d80ed868b7bff1cfacf3
Emmie C <z5360408@ad.unsw.edu.au>
2023-06-03 14:10:41 +1000

    Added a route for tag/details

--- server/routes/tagRoute.ts
+++ server/routes/tagRoute.ts
@@ -1,11 +1,16 @@
 import express from "express";
-import { tagCreate, tagEdit, tagDelete } from "../src/tag";
+import { tagCreate, tagDetails, tagEdit, tagDelete } from "../src/tag";
 
 const router = express.Router();
 
 router.post("/create", (req, res) => {
     const { tagName, colour, textColour } = req.body;
     return res.json(tagCreate(tagName, colour, textColour));
+});
+
+router.get("/details", (req, res) => {
+    const { tagId } = req.body;
+    return res.json(tagDetails(tagId));
 });
 
 router.put("/edit", (req, res) => {
